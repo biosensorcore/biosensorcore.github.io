75955ec442babd00c5967bc337a2f4e08919e66f
biosensorcore <biosensorcore@users.noreply.github.com>
2025-10-09 14:20:45 -0700

    Update Rates.jsx content

--- frontend/src/content/Rates.jsx
+++ frontend/src/content/Rates.jsx
@@ -61,7 +61,7 @@ function Rates() {
                     </tbody>
                 </table>
                 <p className={styles.table_footnote}>* Rates are provisional and subject to change.</p>
-                <p className={styles.table_footnote}>** An initial 30-minute meeting is complimentary for all Academic users.</p>
+                <p className={styles.table_footnote}>** An initial 30-minute meeting is complimentary for all academic users.</p>
             </div>
             
             <div className={content.content_default}>
