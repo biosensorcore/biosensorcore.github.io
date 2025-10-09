52b5a43379aa758bf9d5859fdffb15466485c7d4
biosensorcore <biosensorcore@users.noreply.github.com>
2025-10-09 11:02:30 -0700

    Add UCSD, Non-UCSD Academic, and Commercial rate columns with overhead calculations

--- frontend/src/content/Rates.jsx
+++ frontend/src/content/Rates.jsx
@@ -22,29 +22,41 @@ function Rates() {
                     <thead>
                         <tr>
                             <th>Service Type</th>
-                            <th>Hourly Rate</th>
+                            <th>UCSD Rate</th>
+                            <th>Non-UCSD Academic Rate</th>
+                            <th>Commercial Rate</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>Personnel - Consultation</td>
                             <td>$100/hr</td>
+                            <td>$145/hr</td>
+                            <td>$250/hr</td>
                         </tr>
                         <tr>
                             <td>Personnel - Design & Analysis</td>
                             <td>$100/hr</td>
+                            <td>$145/hr</td>
+                            <td>$250/hr</td>
                         </tr>
                         <tr>
                             <td>Wet Lab</td>
                             <td>$100/hr</td>
+                            <td>$145/hr</td>
+                            <td>$250/hr</td>
                         </tr>
                         <tr>
                             <td>Imaging (full-service)</td>
                             <td>$150/hr</td>
+                            <td>$218/hr</td>
+                            <td>$375/hr</td>
                         </tr>
                         <tr>
                             <td>Imaging (Nikon Imaging Center)</td>
                             <td><a href="https://nic.ucsd.edu/rates/index.html" target="_blank" rel="noopener noreferrer">https://nic.ucsd.edu/rates/index.html</a></td>
+                            <td>-</td>
+                            <td>-</td>
                         </tr>
                     </tbody>
                 </table>
@@ -57,33 +69,47 @@ function Rates() {
                     <thead>
                         <tr>
                             <th>Project Type</th>
-                            <th>Total Cost Range (Hourly Charges Only)</th>
+                            <th>UCSD Rate</th>
+                            <th>Non-UCSD Academic Rate</th>
+                            <th>Commercial Rate</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr id="consultation">
                             <td>Consultation</td>
                             <td>$100 - $400</td>
+                            <td>$145 - $580</td>
+                            <td>$250 - $1,000</td>
                         </tr>
                         <tr id="prototype_design">
                             <td>Prototype Biosensor Design</td>
                             <td>$200 - $500</td>
+                            <td>$290 - $725</td>
+                            <td>$500 - $1,250</td>
                         </tr>
                         <tr id="prototype_construction">
                             <td>Prototype Biosensor Construction</td>
                             <td>$400 - $800</td>
+                            <td>$580 - $1,160</td>
+                            <td>$1,000 - $2,000</td>
                         </tr>
                         <tr id="vitro">
                             <td>Biosensor Characterization (<i>in vitro</i>)</td>
                             <td>$1,200 - $2,600</td>
+                            <td>$1,740 - $3,770</td>
+                            <td>$3,000 - $6,500</td>
                         </tr>
                         <tr id="cellulo">
                             <td>Biosensor Characterization (<i>in cellulo</i>)</td>
                             <td>$1,600 - $3,100</td>
+                            <td>$2,320 - $4,495</td>
+                            <td>$4,000 - $7,750</td>
                         </tr>
                         <tr id="optimization">
                             <td>Full-Scale Biosensor Optimization</td>
                             <td>$2,900 - $5,700</td>
+                            <td>$4,205 - $8,265</td>
+                            <td>$7,250 - $14,250</td>
                         </tr>
                     </tbody>
                 </table>
