bd641e77c5833d3a3d93dbd71c5e2f3d91223195
biosensorcore <biosensorcore@users.noreply.github.com>
2025-10-10 11:24:49 -0700

    Standardize all Resource images to fpbase.png aspect ratio (2.06:1) with white padding

--- frontend/src/content/Resources.jsx
+++ frontend/src/content/Resources.jsx
@@ -7,7 +7,7 @@ function Resources() {
         {
             header: "Biosensor Database",
             text: "The Biosensor Database is a repository of the array of fluorescent biosensors developed to study cell signaling in live cells. This database has been designed to be a community driven resource that can be updated and revised to maintain both a historical and current view of fluorescent biosensor tools. Over time, the database has grown to include a large number of bioluminescent and BRET biosensors as well.",
-            img: "bsdb.jpeg",
+            img: "bsdb_standardized.jpeg",
             url: "https://biosensordb.ucsd.edu/index.php"
         },
         {
@@ -18,20 +18,20 @@ function Resources() {
         },
         {
             header: "BLbase",
-            text: "Work-in-progress database of bioluminescent probes",
-            img: "blbase.png",
+            text: "A work-in-progress database of bioluminescent probes",
+            img: "blbase_standardized.png",
             url: "https://www.blbase.org"
         },
         {
             header: "Bioluminescence Hub",
             text: "Resources for using bioluminescent and bioluminescent-optogenetic (BL-OG) tools",
-            img: "biohub.jpg",
+            img: "biohub_standardized.jpg",
             url: "https://www.bioluminescencehub.org/"
         },
         {
             header: "Cell Signaling San Diego",
             text: "The Cell Signaling Center at UC San Diego unites researchers passionate about understanding the molecular mechanisms of cell signaling with the goal of leveraging this understanding to treat disease.",
-            img: "cssd.png",
+            img: "cssd_standardized.png",
             url: "https://cellsignaling.ucsd.edu/"
         }
     ];
